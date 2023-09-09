fdbc990ac9dd164261bd8d9afe3f3ceae0f599d8
Azuma9280 <kuzuya9280@yahoo.co.jp>
2023-09-09 18:24:01 +0900

    マイグレーションファイル修正2回目

--- database/migrations/2023_09_09_174533_post_user_table.php
+++ database/migrations/2023_09_09_174533_post_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('post_user', function (Blueprint $table) 
         {
         $table->foreignId('post_id')->constrained('posts');     
-        $table->foreignId('user_id')->constrained('users');   
+        $table->foreignId('user_id')->constrained('users');
         $table->primary(['post_id', 'user_id']);  
         });
     }
