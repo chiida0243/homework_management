4f2202876cc2f8bf62d73dc5ced851b072b80fa5
chiida0243 <ririka.n.n.0024@gmail.com>
2023-09-10 16:12:34 +0900

    fix post func

--- database/migrations/2022_11_03_141222_create_posts_table.php
+++ database/migrations/2022_11_03_141222_create_posts_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('title', 50)->nullable();
             $table->string('homework_name',50); //課題名
             $table->string('body', 200)->nullable();
-            $table->foreignId('category_id')->constrained();
+            $table->foreignId('category_id')->nullable();
             $table->string('image_url');  //課題提示用の画像
             $table->foreignId('user_id')->constrained(); //投稿者(先生の名前)
             $table->date('deadline'); //期限日
